ff7795741993eb366826cea5b23cbe00a5f8efaf
isaac <xuhg@dpark.com.cn>
2021-03-05 16:11:46 +0800

    .....

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import './assets/css/base.css'
 import './assets/css/index.css'
-
-function  xx(){
-    return new Promise((resolve,reject)=>{
-        resolve('xxx')
-    })
-}
-
-let yy = await xx()
-console.log(yy)
